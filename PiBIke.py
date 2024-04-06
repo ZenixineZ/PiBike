fa811ec6e4c7fd8e6213bf90c13c2eaa3354f04b
Zane Phillips <zane.phillips@3db-labs.com>
2024-04-06 18:19:44 -0400

    next

--- PiBIke.py
+++ PiBIke.py
@@ -6,5 +6,23 @@ Created on Sat Apr  6 16:11:10 2024
 @author: zenix
 """
 
-import PyQt5
+from PyQt5 import QWidgets
+from PyQt5.QWidgets import QApplication, QMainWindow
+import sys
 
+class PiBike(QMainWindow):
+    def __init__(self):
+        pass
+        
+
+
+
+def window(w, h):
+    app = QApplication(sys.argv)
+    pibike = PiBike()
+    pibike.show()
+    pibike.setGeometry(0, 0, w, h)
+    
+    
+
+    
